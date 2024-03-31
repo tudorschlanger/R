6f0ad41fe7622f393e90cb1967f8521d44acb10e
Tudor Schlanger <tudor.schlanger@gmail.com>
2024-03-31 19:33:10 -0400

    Update master.r

--- template_project/master.r
+++ template_project/master.r
@@ -1,19 +1,41 @@
 ##### Master file ######
 
-## Setting the working directory relative to the base folder of the Project directory
-    install.packages("devtools")
-    install.packages("here")
-    library(here, devtools)
-    here()
 
-## Load all files in the src folder ; this folder contains useful custom-made R functions 
-    
-    # Load all necessary functions in the src folder 
-    packages <- c("miceadds")
-    source(here("code/src/check_packages.r"))
-    check_packages(packages)
-    
-    source.all(here("code/src"), grepstring = "\\.r", print.source = TRUE, file_sep="")
-    
+## Define some paths based on username
+info <- Sys.info()
+
+if (info["user"] == "schl") {
+  output_graphs <- ""
+  output_tables <- ""
+} else if (info["user"] == "") {
+  
+}
+
+
+## Run these at the beginning of the project   
+# install.packages("renv")
+# library(renv)
+# renv::init()
+
+
+## Run this from time to time to lock a set of packages 
+# renv::snapshot()
+
+## Setting the working directory relative to the base folder of the Project directory\
+install.packages("here")
+library(here)
+packages <- c("devtools", "tidyverse", "tibble", "collapse", 
+              "ggplot2", "readxl", "stringr", "psych", "labelled", "fastDummies", 
+              "jsonlite", "stargazer", "xtable", "knitr", "kableExtra", "lubridate", 
+              "patchwork", "fixest", "readr")
+source(here("code/src/check_packages.r"))
+check_packages(packages)
+
+## Load other packages using devtools
+# Align Assign is an Addin for Rstudio which allows user to align equal signs 
+devtools::install_github("seasmith/AlignAssign")
+
+source(here("code/src/functions.R"))
+
 
 
